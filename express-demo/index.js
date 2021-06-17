2f2a961100dad978ca214c0d9f1563d1f869ea4f
Piyush Pandey <piyushpandey379@gmail.com>
2021-06-17 12:53:06 +0530

    GET request

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,14 +1,25 @@
 const express = require('express') ;
-
 const app = express() ;
 
+var courses = [
+    {id: 1 , name: "course1"},
+    {id: 2 , name: "course2"},
+    {id: 3 , name: "course3"},
+    {id: 4 , name: "course4"}
+]
 app.get('/' , (req , res) => {
     res.send("Hello World!!!") ;
 })
 
-app.get('/api/posts/:year/:moth' , (req , res) => {
-    res.send(req.params) ;
-    // res.send(req.query) ;
+app.get('/api/courses/:id' , (req , res) => {
+    const course = courses.find(c => c.id === parseInt(req.params.id)) ;
+
+    if(!course){
+        res.status(404).send("Course Not Found") ;
+    }
+    else{
+        res.send(course) ;
+    }
 })
 
 // Enviroment Variable PORT
